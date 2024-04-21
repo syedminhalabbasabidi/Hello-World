52664438958b954bdf5f32eec592c9f257284dbd
SyedMinhalAbbasAbidi <164239660+syedminhalabbasabidi@users.noreply.github.com>
2024-04-22 04:00:17 +0500

    Add files via upload

---
+++ helloworld/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//let email: "syed@gmail.com"
+//let password= "Minhal;"
+//if (password.length >=6){
+//  console.log ("your password is vaild login successfully")
+//}
+//else (password.length <=5);{
+//console.log ("your password must be in 6 digits")
+//}
+//let age= 18
+//console.log(age < 18, "school")
+//console.log(age >= 18, "university")
+// let KE=true
+// let UPS=true
+// let BULB=KE ||UPS
